4e76d92c2d677c37b02e4eacc212f19dc7274873
MINKYEONG <wkdalsrud43@gmail.com>
2022-02-16 00:09:56 +0900

    modify design

--- _posts/frontend/2022-02-15-firstPost.md
+++ _posts/frontend/2022-02-15-firstPost.md
@@ -2,13 +2,13 @@
 title:  "첫번째 게시물" 
 
 categories:
-  - frontend
+  - Frontend
 tags:
   - [java, git, github, til]
 
-toc: true
-toc_sticky: true
+date: 2022-02-15
 
-date: 2020-07-17
-last_modified_at: 2020-07-17
 ---
+
+
+첫번째 포스트
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,24 +1,24 @@
 var store = [{
         "title": "첫번째 게시물",
-        "excerpt":" ","categories": ["frontend"],
+        "excerpt":"첫번째 포스트  ","categories": ["Frontend"],
         "tags": ["java","git","github","til"],
         "url": "http://localhost:4000/frontend/firstPost/",
         "teaser": null
       },{
         "title": "두번째 게시물",
-        "excerpt":"enqjsWo 포스트  ","categories": ["git"],
+        "excerpt":"enqjsWo 포스트  ","categories": ["Git & Github"],
         "tags": ["git","github","til"],
-        "url": "http://localhost:4000/git/second/",
+        "url": "http://localhost:4000/git%20&%20github/second/",
         "teaser": null
       },{
         "title": "세번째 게시물",
-        "excerpt":"첫번째 포스트  ","categories": ["java"],
+        "excerpt":"세번째  ","categories": ["JAVA"],
         "tags": ["java","git","github","til"],
         "url": "http://localhost:4000/java/dmdkd/",
         "teaser": null
       },{
         "title": "네번째 게시물",
-        "excerpt":"첫번째 포스트  ","categories": ["til"],
+        "excerpt":"두번째 게시물  ","categories": ["TIL"],
         "tags": ["java","git","github","til"],
         "url": "http://localhost:4000/til/djkfls/",
         "teaser": null
